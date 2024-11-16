4c878deae565d3c6d60eaf3fad2dccd2f6fa5272
alan <alanwengrzee@gmail.com>
2024-11-15 21:19:27 -0300

    :lipstick: feat: styles to certificate card finished

--- src/components/CertificateHoverCard.tsx
+++ src/components/CertificateHoverCard.tsx
@@ -10,16 +10,21 @@ interface CertificateHoverCardProps {
 export function CertificateHoverCard({certificate}: CertificateHoverCardProps) {
   return(
     <HoverCard>
-      <HoverCardTrigger className="hidden lg:block cursor-pointer p-4 bg-inherit rounded-full shadow-inner shadow-slate-600 hover:scale-105 duration-500 hover:saturate-50">
+      <HoverCardTrigger className="relative hidden lg:block cursor-pointer p-4 bg-inherit rounded-full shadow-inner shadow-slate-600 hover:scale-105 duration-500 hover:saturate-50">
         <FaGraduationCap />
       </HoverCardTrigger>
-      <HoverCardContent className="cardBorder rounded-md flex justify-between w-auto bg-background p-4">
-        <div className="flex flex-col gap-2">
+      <HoverCardContent className="cardBorder absolute w-max p-4 mt-2 rounded-md flex justify-between  bg-background ">
+        <div className="w-full flex flex-col gap-2">
           <h3 className="text-white text-opacity-90">{certificate.title}</h3>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laboriosam, porro molestiae repudiandae cum, laudantium, aspernatur aliquid corrupti deserunt dolores commodi debitis! Reprehenderit ea atque dignissimos, maxime ut tempore unde!</p>
-          <p>{certificate.institution}</p>
+          <p className="w-96"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laboriosam, porro molestiae repudiandae cum, laudantium, aspernatur aliquid corrupti deserunt dolores commodi debitis! Reprehenderit ea atque dignissimos, maxime ut tempore unde!</p>
+          <h2>{certificate.institution}</h2>
+          <a 
+            href={certificate.link} 
+            target="_blank"
+            className="flex items-center justify-end gap-2"
+          >Ver certificado <FaExternalLinkAlt /></a>
         </div>
-        <a href={certificate.link} target="_blank">Ver certificado <FaExternalLinkAlt /></a>
+        
       </HoverCardContent>
     </HoverCard> 
   )
--- src/data/CERTIFICATE.ts
+++ src/data/CERTIFICATE.ts
@@ -11,5 +11,17 @@ export const CERTIFICATE = [
     title: "Aprofundando em Hooks",
     institution: "Rocketseat",
     link: "https://app.rocketseat.com.br/certificates/12aff34d-9c84-45b5-8ac1-821b315c0b68"
+  },
+  {
+    id: Math.random().toString(),
+    title: "Back-end com Node.js",
+    institution: "Rocketseat",
+    link: "https://app.rocketseat.com.br/certificates/bbf33fc1-dcbf-41e0-b2a3-b412843ee62e"
+  },
+  {
+    id: Math.random().toString(),
+    title: "Aprofundando em Hooks",
+    institution: "Rocketseat",
+    link: "https://app.rocketseat.com.br/certificates/12aff34d-9c84-45b5-8ac1-821b315c0b68"
   }
 ]
