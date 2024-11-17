0cc54536d8e6af13004f5518654d6f260930429c
alan <alanwengrzee@gmail.com>
2024-11-17 20:18:23 -0300

    :zap: perf: optimizing load of fonts

--- src/page/AboutMe/MyselfHeader.tsx
+++ src/page/AboutMe/MyselfHeader.tsx
@@ -3,7 +3,7 @@ export function MyselfHeader() {
     <header className="border-e-2 border-orange-100 w-fit mx-auto p-4 bg-inherit rounded-full shadow-inner shadow-slate-600">
       <img 
         src="https://github.com/AlanWengrze.png" alt="Foto de Alan Wengrze" 
-        className="w-40  border-opacity-80 rounded-full  filter brightness-50 hover:grayscale duration-500"
+        className="w-40 h-40  border-opacity-80 rounded-full  filter brightness-50 hover:grayscale duration-500"
       />
     </header>
   )
